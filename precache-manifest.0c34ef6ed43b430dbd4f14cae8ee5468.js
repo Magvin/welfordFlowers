17a7b706a4c7bf6435d9e82b6ba5a3af4a14e885
magvin <zans.litinskis@gmail.com>
2019-01-18 23:54:16 +0000

    Updates

---
+++ precache-manifest.0c34ef6ed43b430dbd4f14cae8ee5468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dcc50f7ba8667c6afd52018b47e64b9",
+    "url": "/welfordFlowers/static/media/search.7dcc50f7.svg"
+  },
+  {
+    "revision": "39a15ecaaa47d68a037b6885dfe1f37f",
+    "url": "/welfordFlowers/static/media/cart.39a15eca.svg"
+  },
+  {
+    "revision": "cab0ec81c4470a4a517d",
+    "url": "/welfordFlowers/static/js/runtime~main.cab0ec81.js"
+  },
+  {
+    "revision": "7c836558f50b29e23a05",
+    "url": "/welfordFlowers/static/js/main.7c836558.chunk.js"
+  },
+  {
+    "revision": "916b373cfef80c85a28b",
+    "url": "/welfordFlowers/static/js/1.916b373c.chunk.js"
+  },
+  {
+    "revision": "7c836558f50b29e23a05",
+    "url": "/welfordFlowers/static/css/main.ddfd42ab.chunk.css"
+  },
+  {
+    "revision": "115d2515455f1dc523c3238d8efbd0e1",
+    "url": "/welfordFlowers/index.html"
+  }
+];
